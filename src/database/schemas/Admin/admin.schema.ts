74af95ccb25fe6121d3163fa10444b7fb4756c5d
roman-laptop <roman.calderin.25@gmail.com>
2022-12-18 15:24:40 -0500

    :sparkles: database

---
+++ src/database/schemas/Admin/admin.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type AdminDocument = HydratedDocument<Admin>;
+
+@Schema()
+export class Admin {
+  @Prop()
+  name_admin: string;
+  @Prop()
+  position: string;
+  @Prop()
+  phone: string;
+  @Prop()
+  role: string;
+}
+export const AdminSchema = SchemaFactory.createForClass(Admin);
